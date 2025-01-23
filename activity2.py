a5c820fe5981d07488ad79f27078a71d1c461b51
Ricaring <quintillanrica0725@gmail.com>
2025-01-23 21:18:58 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,3 @@
+num1 = eval(input("Please enter a number: "))
+    num2 = eval(input("Please enter another number: "))
+    print(f"{num1} Floor divided to {num2} = {num1 // num2}")
---
+++ activity8.py
@@ -0,0 +1,17 @@
+password = input("Please input your password--->")
+
+if password.lower() == "jewel06":
+	       print("Access Granted, Please continue using the system.")
+	       print("	-----System exit-----")
+elif password.lower() == "Cayrey":
+	       print("Access Granted, Please continue using the system.")
+	       print("	-----System exit-----")
+elif password.lower() == "102504":
+	       print("Access Granted, Please continue using the system.")
+	       print("	-----System exit-----")
+elif password.lower() == "password":
+	       print("Access Granted, Please continue using the system.")
+	       print("	-----System exit-----")
+else:
+	       print("Wrong password, Please try again.")
+	       print("	-----System exit-----")
